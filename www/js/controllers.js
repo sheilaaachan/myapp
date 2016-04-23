e21a57c814c250b674b73edbf4048b8bebac3545
Sheila <sheilaaachan@gmail.com>
2016-04-22 17:14:40 -0700

    styled colors for projects page, added inactive styling

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,14 @@
 angular.module('app.controllers', [])
 
 .controller('projectsCtrl', function($scope) {
-  $scope.projlist = ["H1-B", "Angola Business Visa", "China Business Visa", "Some Other Business Visa"];
+  $scope.active_projs = [
+    ["H1-B", 60, "H1-B description"],
+    ["Angola Business Visa", 20, "Angola Business Visa description"],
+    ["China Business Visa",80,"Hello World"],
+    ["Some Other Business Visa",75,"Hello World"]];
+  $scope.inactive_projs = [
+    ["OPT Stem", 100, "OPT Stem description"],
+    ["Australia Business Visa", 100, "Australia Business Visa description"]];
 })
 
 .controller('projectDetailsCtrl', function($scope) {
